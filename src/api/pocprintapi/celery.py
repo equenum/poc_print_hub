3356436b314071730851ce6462732c3258755d37
Egor Quenum <georgequenum@yahoo.com>
2025-10-23 22:07:07 +0400

    Remove dead letter task

--- src/api/pocprintapi/celery.py
+++ src/api/pocprintapi/celery.py
@@ -3,6 +3,7 @@ import os
 from django.conf import settings
 from celery import Celery
 from celery.exceptions import MaxRetriesExceededError
+from pocprintapi.services.printservice import PrintService
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pocprintapi.settings')
 
@@ -17,13 +18,7 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
     sender.add_periodic_task(
         settings.POC_PRINT_HUB_QUEUE_SCHEDULE_SEC, 
         process_queue_messages.s(), 
-        name=f'process queue messages every {settings.POC_PRINT_HUB_QUEUE_SCHEDULE_SEC}s'
-    )
-
-    sender.add_periodic_task(
-        settings.POC_PRINT_HUB_DEAD_QUEUE_SCHEDULE_SEC, 
-        process_dead_queue_messages.s(), 
-        name=f'process dead queue messages every {settings.POC_PRINT_HUB_DEAD_QUEUE_SCHEDULE_SEC}s'
+        name=f'Process print queue messages every {settings.POC_PRINT_HUB_QUEUE_SCHEDULE_SEC}s'
     )
 
 @app.task(
@@ -34,23 +29,8 @@ def setup_periodic_tasks(sender: Celery, **kwargs):
 )
 def process_queue_messages(self):
     try:
-        print("Processing queue messages")
-    except Exception as ex:
-        try:
-            print(f"Retrying task: {self.name}")
-            self.retry(exc=ex)
-        except MaxRetriesExceededError:
-            print(f"Max task retries hit: {self.name}")
-
-@app.task(
-    bind=True, 
-    ignore_result=True, 
-    max_retries=settings.POC_PRINT_HUB_DEAD_QUEUE_MAX_RETRIES, 
-    default_retry_delay=settings.POC_PRINT_HUB_DEAD_QUEUE_RETRY_DELAY_SEC
-)
-def process_dead_queue_messages(self):
-    try:
-        print("Processing dead queue messages")
+        print("Processing print queue messages")
+        PrintService().process_queue_messages()
     except Exception as ex:
         try:
             print(f"Retrying task: {self.name}")
