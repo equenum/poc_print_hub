ac492823e7731ca3cf1e6d6734fb3e658cc60702
Egor Quenum <georgequenum@yahoo.com>
2025-10-27 22:21:16 +0400

    Add default settings values for optional ones

--- src/api/pocprintapi/settings.py
+++ src/api/pocprintapi/settings.py
@@ -5,40 +5,32 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.environ.get('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG') == 'True'
-POC_PRINT_HUB_TENANT_AUTH_ENABLED = os.environ.get('POC_PRINT_HUB_TENANT_AUTH_ENABLED') == 'True'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'by-using-default-secrets-I-am-running-away-from-my-responsibilities-and-it-feels-good') # - Michael Scott =)
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+
+POC_PRINT_HUB_TENANT_AUTH_ENABLED = os.environ.get('POC_PRINT_HUB_TENANT_AUTH_ENABLED', 'False') == 'True'
 POC_PRINT_HUB_TENANT_ID_HEADER = 'PPH-Tenant-Id'
 POC_PRINT_HUB_TENANT_TOKEN_HEADER = 'PPH-Tenant-Token'
 
-
-# rabbit mq
 POC_PRINT_HUB_RABBIT_MQ_HOST = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_HOST')
 POC_PRINT_HUB_RABBIT_MQ_USERNAME = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_USERNAME')
 POC_PRINT_HUB_RABBIT_MQ_PASSWORD = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_PASSWORD')
+POC_PRINT_HUB_RABBIT_MQ_QUEUE_NAME = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_NAME', 'poc_print_hub')
+POC_PRINT_HUB_RABBIT_MQ_QUEUE_DURABLE = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_DURABLE', 'True') == 'True'
+POC_PRINT_HUB_RABBIT_MQ_QUEUE_BATCH_SIZE = int(os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_BATCH_SIZE', '10'))
+POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_NAME = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_NAME', 'poc_print_hub_dead_letter')
+POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_DURABLE = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_DURABLE', 'True') == 'True'
 
-## print queue
-POC_PRINT_HUB_RABBIT_MQ_QUEUE_NAME = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_NAME')
-POC_PRINT_HUB_RABBIT_MQ_QUEUE_DURABLE = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_DURABLE') == 'True'
-POC_PRINT_HUB_RABBIT_MQ_QUEUE_BATCH_SIZE = int(os.environ.get('POC_PRINT_HUB_RABBIT_MQ_QUEUE_BATCH_SIZE'))
-POC_PRINT_HUB_QUEUE_SCHEDULE_SEC = float(os.environ.get('POC_PRINT_HUB_QUEUE_SCHEDULE_SEC'))
-POC_PRINT_HUB_QUEUE_MAX_RETRIES = int(os.environ.get('POC_PRINT_HUB_QUEUE_MAX_RETRIES'))
-POC_PRINT_HUB_QUEUE_RETRY_DELAY_SEC = int(os.environ.get('POC_PRINT_HUB_QUEUE_RETRY_DELAY_SEC'))
+POC_PRINT_HUB_QUEUE_SCHEDULE_SEC = float(os.environ.get('POC_PRINT_HUB_QUEUE_SCHEDULE_SEC', '5.0'))
+POC_PRINT_HUB_QUEUE_MAX_RETRIES = int(os.environ.get('POC_PRINT_HUB_QUEUE_MAX_RETRIES', '3'))
+POC_PRINT_HUB_QUEUE_RETRY_DELAY_SEC = int(os.environ.get('POC_PRINT_HUB_QUEUE_RETRY_DELAY_SEC', '3'))
+POC_PRINT_HUB_QUEUE_TIME_LIMIT_SEC = int(os.environ.get('POC_PRINT_HUB_QUEUE_TIME_LIMIT_SEC', '300'))
 
-## dead letter queue
-POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_NAME = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_NAME')
-POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_DURABLE = os.environ.get('POC_PRINT_HUB_RABBIT_MQ_DEAD_QUEUE_DURABLE') == 'True'
-
-
-# printer
 POC_PRINT_HUB_PRINTER_HOST = os.environ.get('POC_PRINT_HUB_PRINTER_HOST')
 POC_PRINT_HUB_PRINTER_MESSAGE_SEPARATOR = os.environ.get('POC_PRINT_HUB_PRINTER_MESSAGE_SEPARATOR', '----------')
 POC_PRINT_HUB_PRINTER_CHECK_PAPER_STATUS = os.environ.get('POC_PRINT_HUB_PRINTER_CHECK_PAPER_STATUS', 'True') == 'True'
 
-
-# celery
 CELERY_BROKER_URL = f"amqp://{POC_PRINT_HUB_RABBIT_MQ_USERNAME}:{POC_PRINT_HUB_RABBIT_MQ_PASSWORD}@{POC_PRINT_HUB_RABBIT_MQ_HOST}:5672//"
-POC_PRINT_HUB_QUEUE_TIME_LIMIT_SEC = int(os.environ.get('POC_PRINT_HUB_QUEUE_TIME_LIMIT_SEC'))
 
 ALLOWED_HOSTS = []
 
@@ -85,11 +77,11 @@ WSGI_APPLICATION = 'pocprintapi.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get('DATABASES_POSTGRESQL_NAME'),
+        "NAME": os.environ.get('DATABASES_POSTGRESQL_NAME', 'poc_printer_hub'),
         "USER": os.environ.get('DATABASES_POSTGRESQL_USER'),
         "PASSWORD": os.environ.get('DATABASES_POSTGRESQL_PASSWORD'),
         "HOST": os.environ.get('DATABASES_POSTGRESQL_HOST'),
-        "PORT": os.environ.get('DATABASES_POSTGRESQL_PORT'),
+        "PORT": os.environ.get('DATABASES_POSTGRESQL_PORT', '5432'),
         'OPTIONS': {
             'options': '-c search_path=pocprinthub,public'
         },
