d494694f6abb69091c7e043f7aa3c504c81be3e6
Egor Quenum <georgequenum@yahoo.com>
2025-11-24 03:10:31 +0400

    UI Wire up tenant auth

--- src/web-ui/poc-print-ui/src/app/app.ts
+++ src/web-ui/poc-print-ui/src/app/app.ts
@@ -1,13 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-// extract
-interface SelectOption {
-  value: string;
-  displayName: string;
-}
+import { PrinterStatusData, QueueStatusData, SelectOption, TenantData } from './interfaces';
+import { ApiService } from './services/api.service';
+import { TenantService } from './services/tenant.service';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +12,10 @@ interface SelectOption {
   templateUrl: './app.html'
 })
 export class App {
-  protected readonly title = signal('poc-print-ui');
+  public readonly dataPlaceholder = 'N/A';
+
+  private readonly apiService = inject(ApiService);
+  private readonly tenantService = inject(TenantService);
 
   readonly bodyTypeOptions: SelectOption[] = [
     { value: 'text', displayName: 'Text' },
@@ -27,10 +27,47 @@ export class App {
     { value: 'custom', displayName: 'Custom' }
   ];
 
+  // control inputs
   selectedBodyType = signal<string>('text');
   selectedFeedSelectorType = signal<string>('slider');
   selectedFeedTimes = signal<number>(5);
-  
+
+  // tenant auth
+  isTenantAuthenticated = signal<boolean>(false);
+  isTenantAuthInProgress = signal<boolean>(false);
+
+  tenantId = signal<string>('');
+  tenantToken = signal<string>('');
+
+  // dashboard status
+  isQueueStatusLoaded = signal<boolean>(false);
+  isPrinterStatusLoaded = signal<boolean>(false);
+
+  // dashboard data
+  queueStatusData: QueueStatusData | undefined;
+  printerStatusData: PrinterStatusData | undefined;
+  tenantData: TenantData | undefined;
+
+  async onTenantAuthSave(): Promise<void> {
+    this.isTenantAuthInProgress.set(true);
+
+    var tenantData = await this.tenantService.authenticate(
+      this.tenantId().trim(), 
+      this.tenantToken().trim()
+    );
+    
+    if (!tenantData) {
+      // show error toast: ngx-toastr
+      this.isTenantAuthInProgress.set(false);
+      return;
+    }
+
+    this.tenantData = tenantData;
+    this.isTenantAuthenticated.set(true);
+    this.isTenantAuthInProgress.set(false);
+    // show success toast: ngx-toastr
+  }
+
   getBodyTypePlaceholder(): string {
     switch (this.selectedBodyType()) {
       case 'text':
@@ -40,5 +77,82 @@ export class App {
       default:
         return 'Enter message text';
     }
+  }
+
+  getPrinterName(): string {
+    return this.printerStatusData
+      ? this.printerStatusData.name 
+      : this.dataPlaceholder;
+  }
+
+  getPrinterStatus(): string {
+    return this.printerStatusData
+      ? this.getQueueStatusMesage(this.printerStatusData.isOnline) 
+      : this.dataPlaceholder;
+  }
+
+  getPrinterPaperStatus(): string {
+    return this.printerStatusData
+      ? this.printerStatusData.paperStatus 
+      : this.dataPlaceholder;
+  }
+
+  getQueueStatus(name: string): string {
+    if (this.queueStatusData) {
+      switch (name) {
+        case 'print':
+          return this.getQueueStatusMesage(this.queueStatusData.print.isOnline);
+        case 'error':
+          return this.getQueueStatusMesage(this.queueStatusData.deadLetter.isOnline);
+        default:
+          return this.dataPlaceholder;
+      }
+    }
+
+    return this.dataPlaceholder;
+  }
+
+  getQueueCount(name: string): string {
+    if (this.queueStatusData) {
+      switch (name) {
+        case 'print':
+          return this.queueStatusData.print.count.toString();
+        case 'error':
+          return this.queueStatusData.deadLetter.count.toString();
+        default:
+          return this.dataPlaceholder;
+      }
+    }
+
+    return this.dataPlaceholder;
+  }
+
+  getTenantId(): string {
+    if (this.tenantData) {
+      return this.tenantData.tenantId.length >= 10
+        ? `${this.tenantData.tenantId.slice(0, 10)}***` 
+        : this.tenantData.tenantId
+    }
+
+    return this.dataPlaceholder;
+  }
+
+  getTenantRole(): string {
+    if (!this.tenantData) {
+      return this.dataPlaceholder;
+    }
+
+    switch (this.tenantData.role) {
+      case 'ADMIN':
+        return 'Admin';
+      case 'USER':
+        return 'User';
+      default:
+        return this.dataPlaceholder;
+    }
+  }
+
+  getQueueStatusMesage(isOnline: boolean): string {
+    return isOnline ? 'Online' : 'Offline';
   }
 }
