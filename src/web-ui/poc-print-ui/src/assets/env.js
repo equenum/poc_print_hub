303ddb6b3001678e2c8571f98f37c81fefdd41bf
Egor Quenum <georgequenum@yahoo.com>
2025-12-20 16:06:23 +0400

    UI Remove env.js

--- src/web-ui/poc-print-ui/src/assets/env.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  window["env"] = window["env"] || {};
-
-  window["env"]["isProduction"] = false;
-  window["env"]["apiUrl"] = "http://localhost:8000/api";
-  window["env"]["tenantIdHeader"] = "Pph-Tenant-Id";
-  window["env"]["tenantTokenHeader"] = "Pph-Tenant-Token";
-  window["env"]["messageOriginName"] = "print-hub-web-ui";
-})(this);
